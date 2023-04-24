12d43f2cc1efdf84e8e4947a2a5f28f851a97903
Divyansh62 <64961736+Divyansh62@users.noreply.github.com>
2023-04-24 19:43:43 +0530

    first Commit

---
+++ src/Data/QuizData.js
@@ -0,0 +1,26 @@
+export const QuizData=[
+    {
+        question:"What is the Capital of India",
+        options:["Delhi","Mumbai","Chennai","Kolkata"],
+        answer:1,
+        level:"Easy"
+    },
+    {
+        question:"What is the Capital of India",
+        options:["Delhi","Mumbai","Chennai","Kolkata"],
+        answer:1,
+        level:"Easy"
+    },
+    {
+        question:"What is the Capital of India",
+        options:["Delhi","Mumbai","Chennai","Kolkata"],
+        answer:1,
+        level:"Easy"
+    },
+    {
+        question:"What is the Capital of India",
+        options:["Delhi","Mumbai","Chennai","Kolkata"],
+        answer:1,
+        level:"Easy"
+    }
+]
